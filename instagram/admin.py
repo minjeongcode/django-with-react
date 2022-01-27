7c2aab9e7379fb78feb4f7ff5a0ae199c114e937
Minjeong Bak <doniminp@gmail.com>
2022-01-27 23:49:44 +0900

    django media

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib          import admin
+from django.utils.safestring import mark_safe
 
 from .models        import Post
 
@@ -18,10 +19,14 @@ from .models        import Post
 # list_filter : 지정 필드값으로 필터링 옵션 제공
 @admin.register(Post) # Wrapping
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
+    list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
     list_display_links = ['message']
     list_filter = ['created_at', 'is_public']
     search_fields = ['message']
     
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" style="width: 70px;" />')
+        return None
     def message_length(self, post):
         return f'{len(post.message)} 글자'
