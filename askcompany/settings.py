5a3c5d741392eb73fff02b382aecdf910dd9f61c
Minjeong Bak <doniminp@gmail.com>
2022-02-05 22:42:04 +0900

    accounts app 추가, OneToOneField

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'debug_toolbar',
     # locals apps
+    'accounts',
     'blog1',
     'instagram',
 ]
--- askcompany/urls.py
+++ askcompany/urls.py
@@ -22,6 +22,7 @@ from django.urls    import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts', include('accounts.urls')),
     path('blog1', include('blog1.urls')),
     path('instagram', include('instagram.urls')),
 ]
