a3fa7f6bc821801f046e273519e9134e781c5091
Minjeong Bak <doniminp@gmail.com>
2022-02-07 15:23:58 +0900

    Gerneric display views

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third apps
+    'bootstrap5',
     'django_extensions',
     'debug_toolbar',
     # locals apps
--- instagram/views.py
+++ instagram/views.py
@@ -1,19 +1,21 @@
 from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Post
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
+post_list = ListView.as_view(model=Post, paginate_by=10)
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
     
-    if q:
-        qs = qs.filter(message__icontains=q)
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q,
-    })
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q,
+#     })
 
 # def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
 #     post = get_object_or_404(Post, pk=pk)
@@ -26,7 +28,21 @@ def post_list(request):
 #         'post': post,
 #     })
 
-post_detail = DetailView.as_view(model=Post)
+# post_detail = DetailView.as_view(
+#     model=Post,
+#     queryset=Post.objects.filter(is_public=True))
+
+class PostDetailView(DetailView):
+    model = Post
+    # queryset = Post.objects.filter(is_public=True)
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if not self.request.user.is_authenticated:
+            qs = qs.filter(is_public=True)
+        return qs
+
+post_detail = PostDetailView.as_view()
 
 def archive_year(request, year):
     return HttpResponse(f"{year}년 archives")
